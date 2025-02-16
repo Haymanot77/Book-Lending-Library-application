8b027dcdb14a37e5613de6ecfef07a729ea27dd7
Haymanot <haymigidena1@gmail.com>
2025-02-16 17:34:19 +0300

    Setting up environment

---
+++ db/migrate/20250216133208_create_borrowing_histories.rb
@@ -0,0 +1,12 @@
+class CreateBorrowingHistories < ActiveRecord::Migration[8.0]
+  def change
+    create_table :borrowing_histories do |t|
+      t.references :book, null: false, foreign_key: true
+      t.string :borrower_name
+      t.datetime :borrowed_at
+      t.datetime :returned_at
+
+      t.timestamps
+    end
+  end
+end
