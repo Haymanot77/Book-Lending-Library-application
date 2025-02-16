cd0c16ea30268ba07055b2a03ef62646b701f8da
Haymanot <haymigidena1@gmail.com>
2025-02-16 20:54:26 +0300

    fixed importmap audit

--- app/controllers/borrowing_histories_controller.rb
+++ app/controllers/borrowing_histories_controller.rb
@@ -2,10 +2,9 @@ class BorrowingHistoriesController < ApplicationController
     def index
       @borrowers = BorrowingHistory.select(:borrower_name).distinct
     end
-  
+
     def show
       @borrower_name = params[:id]
       @histories = BorrowingHistory.where(borrower_name: @borrower_name).order(borrowed_at: :desc)
     end
-  end
-  
+end
